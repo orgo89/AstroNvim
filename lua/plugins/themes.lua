c1af710eb9f0e12fa845cf79ea600548b3d4fa75
Juan Diego Lopez Ramirez <orgo_89@hotmail.com>
2025-08-26 20:14:43 -0600

    Added some themes and started implementing java debugger

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,32 @@
+if false then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+return {
+  {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa"
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    name = "nightfox"
+  },
+  {
+    "baliestri/aura-theme",
+    name = "aura"
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine"
+  },
+  {
+    "savq/melange-nvim",
+    name = "melange"
+  },
+  {
+    "luisiacc/gruvbox-baby",
+    name = "gruvbox-baby"
+  },
+  {
+    "Biscuit-Theme/nvim",
+    name = "Biscuit-Theme"
+  }
+}
