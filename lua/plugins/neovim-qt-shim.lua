c4b4844e6d68f5c2436ea92bc1a9ccbfceb7becc
Juan Diego Lopez Ramirez <orgo_89@hotmail.com>
2026-02-11 16:56:08 -0600

    Added plugins - Multiple Cursors, themes, Neovim-Qt

---
+++ lua/plugins/neovim-qt-shim.lua
@@ -0,0 +1,4 @@
+-- Plugin that enables Gui like commands for Neovim-QT (nvim-qt)
+return {
+    "equalsraf/neovim-gui-shim"
+}
--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -52,7 +52,7 @@ return {
     },
     config = function (_, opts)
       vim.o.termguicolors = true
-      vim.o.background = 'light'
+      vim.o.background = 'dark'
       require('solarized').setup(opts)
       vim.cmd.colorscheme 'solarized'
     end
